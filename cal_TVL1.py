e78f78318ba5541a8f551ad25201f8e826899257
zhou-ning <1767508581@qq.com>
2022-07-26 21:47:33 +0800

    fix:光流提取

--- cal_TVL1.py
+++ cal_TVL1.py
@@ -4,74 +4,84 @@ import numpy as np
 import glob
 import multiprocessing
 
-video_root = 'activity_net_train_exist_name_small.txt'
+###### 使用frames帧进行 flow光流计算
+video_root = 'video_list.txt'
 root = 'frames'
-out_root = 'flows/train'
+out_root = 'flow'
+
 
 def cal_for_frames(video_path):
-    #print(video_path)
-    frames = glob.glob(os.path.join(video_path,'*.jpg'))
+    # print(video_path)
+    frames = glob.glob(os.path.join(video_path, '*.jpg'))
     frames.sort()
-    
+
     flow = []
     prev = cv2.imread(frames[0])
-    prev = cv2.cvtColor(prev,cv2.COLOR_BGR2GRAY)
+    prev = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
     for i, frame_curr in enumerate(frames[1:]):
         curr = cv2.imread(frame_curr)
-        curr = cv2.cvtColor(curr,cv2.COLOR_BGR2GRAY)
-        tmp_flow = compute_TVL1(prev,curr)
+        curr = cv2.cvtColor(curr, cv2.COLOR_BGR2GRAY)
+        tmp_flow = compute_TVL1(prev, curr)
         flow.append(tmp_flow)
         prev = curr
-    
+
     return flow
-    
-def compute_TVL1(prev,curr,bound=15):
+
+
+def compute_TVL1(prev, curr, bound=15):
     TVL1 = cv2.optflow.DualTVL1OpticalFlow_create()
-    flow = TVL1.calc(prev,curr,None)
-    
+    flow = TVL1.calc(prev, curr, None)
+
     assert flow.dtype == np.float32
-    
-    flow = (flow+bound)*(255.0/(2*bound))
+
+    flow = (flow + bound) * (255.0 / (2 * bound))
     flow = np.round(flow).astype(int)
-    flow[flow>=255]=255
-    flow[flow<=0]=0
-    
+    flow[flow >= 255] = 255
+    flow[flow <= 0] = 0
+
     return flow
-    
-def save_flow(video_flows,flow_path):
+
+
+def save_flow(video_flows, flow_path):
     if not os.path.exists(flow_path):
         os.mkdir(os.path.join(flow_path))
     for i, flow in enumerate(video_flows):
-        cv2.imwrite(os.path.join(flow_path,str(i)+'_x.jpg'),flow[:,:,0])
-        cv2.imwrite(os.path.join(flow_path,str(i)+'_y.jpg'),flow[:,:,1])
+        cv2.imwrite(os.path.join(flow_path, str(i) + '_x.jpg'), flow[:, :, 0])
+        cv2.imwrite(os.path.join(flow_path, str(i) + '_y.jpg'), flow[:, :, 1])
 
-def process(video_path,flow_path):
+
+def process(video_path, flow_path):
     flow = cal_for_frames(video_path)
     save_flow(flow, flow_path)
 
-def extract_flow(root,out_root):
+
+def extract_flow(root, out_root):
     if not os.path.exists(out_root):
         os.mkdir(out_root)
-    #dir_list = os.listdir(root)
+    # dir_list = os.listdir(root)
     dir_list = []
+    ### 读取txt中视频信息
     with open(video_root, 'r') as f:
         for id, line in enumerate(f):
             video_name = line.strip().split()
-            dir_list.append('v_' + video_name[0])
+            preffix = video_name[0].split('.')[0]
+            dir_list.append(preffix)
 
-    pool = multiprocessing.Pool(processes=1)
+    pool = multiprocessing.Pool(processes=4)
     for dir_name in dir_list:
-        video_path = os.path.join(root,dir_name)
-        flow_path = os.path.join(out_root,dir_name)
+        video_path = os.path.join(root, dir_name)
+        flow_path = os.path.join(out_root, dir_name)
 
-        #flow = cal_for_frames(video_path)
-        #save_flow(flow,flow_path)
-        #print('save flow data: ',flow_path)
+        # flow = cal_for_frames(video_path)
+        # save_flow(flow,flow_path)
+        # print('save flow data: ',flow_path)
         # process(video_path,flow_path)
-        pool.apply_async(process,args=(video_path,flow_path))
+        pool.apply_async(process, args=(video_path, flow_path))
 
     pool.close()
     pool.join()
 
+
 if __name__ == '__main__':
-    extract_flow(root,out_root)
+    extract_flow(root, out_root)
+    print("finish!!!!!!!!!!!!!!!!!!")
--- read_frames.py
+++ read_frames.py
@@ -3,18 +3,19 @@ import numpy as np
 import os
 import multiprocessing
 
-video_root = 'activity_net_train_exist_name_small.txt'
-root = 'origi_videos'
+video_root = 'video_list.txt'
+root = 'videos'
 out_root = 'frames'
 suffix = '.jpg'
 
-def save_image(root,vid_name,num,image):
-    file_name = os.path.join(root,vid_name,str(num)+suffix)
-    #print(file_name)
-    cv2.imwrite(file_name,image)
+
+def save_image(root, vid_name, num, image):
+    file_name = os.path.join(root, vid_name, str(num) + suffix)
+    # print(file_name)
+    cv2.imwrite(file_name, image)
 
 
-def process(vid_path,preffix):
+def process(vid_path, preffix):
     videoCapture = cv2.VideoCapture(vid_path)
 
     i = 0
@@ -22,34 +23,38 @@ def process(vid_path,preffix):
         success, frame = videoCapture.read()
         if success:
             i = i + 1
-            save_image(out_root, preffix, i-1, frame)
-            #print('save image vid name: ', file_name, '; frame num: ', i)
+            save_image(out_root, preffix, i, frame)
+            # print('save image vid name: ', file_name, '; frame num: ', i)
         else:
             break
+
 
 def main(root):
     if not os.path.exists(out_root):
         os.mkdir(out_root)
-    #path_list = os.listdir(root)
+    # path_list = os.listdir(root)
     path_list = []
-    with open(video_root,'r') as f:
-        for id,line in enumerate(f):
+    #### 读取txt中视频信息 ####
+    with open(video_root, 'r') as f:
+        for id, line in enumerate(f):
             video_name = line.strip().split()
-            path_list.append(video_name[0]+'.mp4')
+            path_list.append(video_name[0])
 
     pool = multiprocessing.Pool(processes=4)
     for file_name in path_list:
-        path = os.path.join(root,file_name)
+        path = os.path.join(root, file_name)
         preffix = file_name.split('.')[0]
-        dir_name = os.path.join(out_root,preffix)
+        dir_name = os.path.join(out_root, preffix)
         if not os.path.exists(dir_name):
             os.mkdir(dir_name)
 
-        pool.apply_async(process,args=(path,preffix))
-        #process(path,preffix)
+        pool.apply_async(process, args=(path, preffix))
+        # process(path,preffix)
 
     pool.close()
     pool.join()
-        
+
+
 if __name__ == '__main__':
     main(root)
+    print("finish!!!!!!!!!!!!!!!!!!")
